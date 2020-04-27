76ff2b7ba95fad9fa3757faf011c32cd1eb6f948
linzhixiang <linborris@163.com>
2020-04-27 18:00:42 +0800

    v1.0.0 初始化

---
+++ IdeaProjects/springcloud20/cloud-eureka-server7001/src/main/java/com/atguigu/springcloud/EurekaMain7001.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Author lzx
+ * @date 2020/4/27 15:11
+ * @Description TODO
+ **/
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7001 {
+    public static void main(String[] args){
+        SpringApplication.run(EurekaMain7001.class, args);
+    }
+}
