c41e08b1c57ed6c57e62b9eee99a73f5a97fb3e0
linborris <linborris@163.com>
2020-05-02 12:34:54 +0800

    v1.0.0 consul

---
+++ IdeaProjects/springcloud20/cloud-providerconsul-payment8006/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -0,0 +1,20 @@
+package com.atguigu.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@Slf4j
+public class PaymentController {
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping("/payment/consul")
+    public String paymentConsul(){
+        return "spring cloud with consul"+serverPort+"\t"+ UUID.randomUUID().toString();
+    }
+}
