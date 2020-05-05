34e733c96a7c15f63f623e2ffd1cae53db298fe4
linborris <linborris@163.com>
2020-05-05 13:46:53 +0800

    v1.0.0 openfeign

---
+++ IdeaProjects/springcloud20/cloud-consumer-feign-order80/src/main/java/com/atguigu/springclou/OrderFeignMain80.java
@@ -0,0 +1,16 @@
+package com.atguigu.springclou;
+
+import com.atguigu.myrule.MySelfRule;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+@RibbonClient(name = "CLOUD-PAYMENT-SERVICE",configuration = MySelfRule.class)
+public class OrderFeignMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderFeignMain80.class, args);
+    }
+}
---
+++ IdeaProjects/springcloud20/cloud-consumer-feign-order80/src/main/java/com/atguigu/springclou/service/PaymentFeignService.java
@@ -0,0 +1,17 @@
+package com.atguigu.springclou.service;
+
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
+import feign.Param;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Service
+@FeignClient(value="CLOUD-PAYMENT-SERVICE")
+public interface PaymentFeignService {
+
+    @GetMapping("/payment/get/{id}")
+    CommonResult<Payment> getPaymentById(@PathVariable("id") Long id);
+}
